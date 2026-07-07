8a4a92db54dad1da4b35359cb2d6d073f2be4854
Shiroizx <kzxxx01@gmail.com>
2026-07-07 10:53:41 +0700

    fix: resolve null pointer reference when hubungi or lokasi sections are not yet loaded

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -31,6 +31,26 @@
     <meta property="twitter:image" content="{{ asset($settings['logo_path'] ?? 'asset/logo.jpeg') }}">
 
     <!-- Structured Data (JSON-LD) -->
+    @php
+        $hubungiSection = $sections['hubungi_kami'] ?? null;
+        $lokasiSection = $sections['lokasi'] ?? null;
+
+        $phoneNum = ($hubungiSection && isset($hubungiSection->extra['phone']))
+            ? str_replace(["\r", "\n"], ", ", trim($hubungiSection->extra['phone']))
+            : '+628138287909';
+
+        $addressVal = ($lokasiSection && isset($lokasiSection->extra['address']))
+            ? str_replace(["\r", "\n"], " ", trim($lokasiSection->extra['address']))
+            : 'Jl. Swasembada Barat XXV No 19 RT007 RW011, Kebon Bawang, Tj Priok, Jakarta Utara, DKI Jakarta';
+
+        $latitudeVal = ($lokasiSection && isset($lokasiSection->extra['latitude']))
+            ? $lokasiSection->extra['latitude']
+            : -6.116518;
+
+        $longitudeVal = ($lokasiSection && isset($lokasiSection->extra['longitude']))
+            ? $lokasiSection->extra['longitude']
+            : 106.885368;
+    @endphp
     <script type="application/ld+json">
     {
       "@context": "https://schema.org",
@@ -39,10 +59,10 @@
       "image": "{{ asset($settings['logo_path'] ?? 'asset/logo.jpeg') }}",
       "@id": "{{ url('/') }}",
       "url": "{{ url('/') }}",
-      "telephone": "{{ isset($sections['hubungi_kami']->extra['phone']) ? str_replace(["\r", "\n"], ", ", trim($sections['hubungi_kami']->extra['phone'])) : '+628138287909' }}",
+      "telephone": "{{ $phoneNum }}",
       "address": {
         "@type": "PostalAddress",
-        "streetAddress": "Jl. Swasembada Barat XXV No 19 RT007 RW011, Kebon Bawang, Tj Priok",
+        "streetAddress": "{{ $addressVal }}",
         "addressLocality": "Jakarta Utara",
         "addressRegion": "DKI Jakarta",
         "postalCode": "14320",
@@ -50,8 +70,8 @@
       },
       "geo": {
         "@type": "GeoCoordinates",
-        "latitude": {{ $sections['lokasi']->extra['latitude'] ?? -6.116518 }},
-        "longitude": {{ $sections['lokasi']->extra['longitude'] ?? 106.885368 }}
+        "latitude": {{ $latitudeVal }},
+        "longitude": {{ $longitudeVal }}
       },
       "openingHoursSpecification": {
         "@type": "OpeningHoursSpecification",
@@ -69,6 +89,7 @@
       }
     }
     </script>
+
 
     <!-- Google Analytics -->
     @if (!empty($settings['google_analytics_id']))
