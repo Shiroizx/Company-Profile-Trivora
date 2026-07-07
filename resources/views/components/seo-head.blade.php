efc7ce74e34a3f4e07df8f645db1c43d6e957da6
Shiroizx <kzxxx01@gmail.com>
2026-07-07 11:57:12 +0700

    seo: restore standard formatting for google analytics tag

--- resources/views/components/seo-head.blade.php
+++ resources/views/components/seo-head.blade.php
@@ -87,6 +87,13 @@
 </script>
 
 @if($gaId)
+<!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id={{ $gaId }}"></script>
-<script>window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments)}gtag('js',new Date());gtag('config','{{ $gaId }}');</script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', '{{ $gaId }}');
+</script>
 @endif
